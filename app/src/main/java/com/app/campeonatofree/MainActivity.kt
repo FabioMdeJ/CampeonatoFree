c9ab0fc307b7450864eeb638b18940829ac3b17d
FabioMJ <nego.jane3@gmail.com>
2026-02-19 21:07:20 -0300

    adiciona tela criar campeonato

--- app/src/main/java/com/app/campeonatofree/MainActivity.kt
+++ app/src/main/java/com/app/campeonatofree/MainActivity.kt
@@ -1,37 +1,43 @@
 package com.app.campeonatofree
 
-import androidx.navigation.compose.
+//import androidx.navigation.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.app.campeonatofree.ui.theme.CampeonatoFreeTheme
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.app.campeonatofree.ui.theme.CampeonatoFreeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            var telaAtual by remember { mutableStateOf("inicial") }
+
             CampeonatoFreeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                if (telaAtual == "inicial") {
                     TelaInicial(
-                        modifier = Modifier.padding(paddingValues = innerPadding)
+                        onCriarClick = { telaAtual = "criar" }
                     )
+                } else {
+                    TelaCriarCampeonato()
                 }
             }
         }
@@ -40,7 +46,10 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun TelaInicial(modifier: Modifier = Modifier) {
+fun TelaInicial(
+    modifier: Modifier = Modifier,
+    onCriarClick: () -> Unit
+) {
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -48,6 +57,16 @@ fun TelaInicial(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_campenatofree),
+            contentDescription = "Logo Campeonato Free",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp),
+            contentScale = ContentScale.Fit
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+
         Text(
             text = "Campeonato Free",
             fontSize = 24.sp
@@ -55,8 +74,42 @@ fun TelaInicial(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        Button(onClick = { }) {
+        Button(onClick = onCriarClick) {
             Text("Criar Campeonato")
+        }
+    }
+}
+
+@Composable
+fun TelaCriarCampeonato() {
+
+    var nomeCampeonato by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Criar Campeonato",
+            fontSize = 24.sp
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        TextField(
+            value = nomeCampeonato,
+            onValueChange = {nomeCampeonato = it},
+            label = { Text("Nome do campeonato") }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = {}) {
+            Text("Salvar")
         }
     }
 }
